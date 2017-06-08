977294db20342accfde2a3ae716137d3eb33ce5d
vali <valentin_chiriac@yahoo.com>
2017-06-08 11:10:43 +0200

    clean up

---
+++ src/main/java/org/eu/client/domain/RetryTimes.java
@@ -0,0 +1,7 @@
+package org.eu.client.domain;
+
+public interface RetryTimes {
+    int NO_RETRY = 1;
+    int WITH_RETRY = 3;
+
+}
--- src/main/java/org/eu/client/main/MainApp.java
+++ src/main/java/org/eu/client/main/MainApp.java
@@ -2,6 +2,7 @@ package org.eu.client.main;
 
 import org.eu.client.domain.Email;
 import org.eu.client.domain.HtmlEmail;
+import org.eu.client.domain.RetryTimes;
 import org.eu.client.domain.TextEmail;
 import org.eu.client.encryption.AES_EncryptionAlgorithm;
 import org.eu.client.encryption.DES_EncryptionAlgorithm;
@@ -13,30 +14,22 @@ import org.eu.client.mail.InternalEmailService;
 
 /**
  * The emails can be sent as plain text or as HTML
- *  - emails which are sent outside the company servers need to be logged and have a disclaimer added to the end.
- *  What's worse, the emails could also be encrypted with AES or DES,
+ * - emails which are sent outside the company servers need to be logged and have a disclaimer added to the end.
+ * What's worse, the emails could also be encrypted with AES or DES,
  * or even both at the same time.  Your boss tells you that in case there are problems when sending the emails you
  * should retry the sending operation up to three times.
- *
+ * <p/>
  * Design a simple client with the accompanying class(es) which you consider to be necessary and write a short program
  * inside the standard void main(String[] args) function that would implement the following scenarios by making use of
  * your class(es):
-
- ·         sending a plain text email to an outside resource, with a disclaimer added at the end, unencrypted and no retry
- ·         sending an HTML email to an internal server (so without the disclaimer), encrypted with DES, with the retry functionality
- ·         sending an HTML email to an outside resource, with a disclaimer added at the end and encrypted with AES with retries in case of errors
- ·         sending a plain text email to an outside resource and encrypted first with DES and then with AES
- *
- *
- *
+ * <p/>
+ * ·         sending a plain text email to an outside resource, with a disclaimer added at the end, unencrypted and no retry
+ * ·         sending an HTML email to an internal server (so without the disclaimer), encrypted with DES, with the retry functionality
+ * ·         sending an HTML email to an outside resource, with a disclaimer added at the end and encrypted with AES with retries in case of errors
+ * ·         sending a plain text email to an outside resource and encrypted first with DES and then with AES
  */
 public class MainApp {
 
-    private final static int NO_RETRY = 1;
-    private final static int WITH_RETRY = 3;
-
-    private EmailService emailService;
-    private Email email;
 
     public static void main(String[] args) {
         try {
@@ -49,25 +42,25 @@ public class MainApp {
     public void run() throws InternalException {
 
         //send a plain text email to an outside resource, with a disclaimer added at the end, unencrypted and no retry
-        emailService = new ExternalEmailService(NO_RETRY);
-        email = new TextEmail();
+        EmailService emailService = new ExternalEmailService(RetryTimes.NO_RETRY);
+        Email email = new TextEmail();
         email.setEncryptionWith(new NO_EncriptionAlgorithm());
         emailService.send(email);
 
         //ending an HTML email to an internal server (so without the disclaimer), encrypted with DES, with the retry functionality
-        emailService = new InternalEmailService(WITH_RETRY);
+        emailService = new InternalEmailService(RetryTimes.WITH_RETRY);
         email = new HtmlEmail();
         email.setEncryptionWith(new DES_EncryptionAlgorithm());
         emailService.send(email);
 
         //sending an HTML email to an outside resource, with a disclaimer added at the end and encrypted with AES with retries in case of errors
-        emailService = new ExternalEmailService(WITH_RETRY);
+        emailService = new ExternalEmailService(RetryTimes.WITH_RETRY);
         email = new HtmlEmail();
         email.setEncryptionWith(new AES_EncryptionAlgorithm());
         emailService.send(email);
 
         //sending a plain text email to an outside resource and encrypted first with DES and then with AES
-        emailService = new ExternalEmailService(NO_RETRY);
+        emailService = new ExternalEmailService(RetryTimes.NO_RETRY);
         email = new HtmlEmail();
         email.setEncryptionWith(new DES_EncryptionAlgorithm(), new AES_EncryptionAlgorithm());
         emailService.send(email);
